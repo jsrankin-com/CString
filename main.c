ff63ee1fdc7f49ef01380d75a5e77d496cbf9362
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-02-15 23:04:31 -0500

    New Release
    Added _toInt(), _toDouble(), and _compare(). 
    Added tests
    Fixed an issue where strings didn't have a slength.

--- main.c
+++ main.c
@@ -12,12 +12,13 @@ typedef struct
 	unsigned id;
 	CharString first;
 	CharString last;
-
+	
 } Person;
 
 inline CharString* _concat(CharString* lhs, CharString* rhs)
 {
 	size_t lhs_length = lhs->length;
+	//Includes \0
 	size_t rhs_length = rhs->length + 1;
 	size_t size = lhs_length + rhs_length;
 	CharString* ret = NULL;
@@ -28,7 +29,7 @@ inline CharString* _concat(CharString* lhs, CharString* rhs)
 		return EXIT_FAILURE;
 	}
 
-	ret->data = malloc(sizeof(char) * size);
+	ret->data = malloc(sizeof(char)*size);
 	if (!ret->data)
 	{
 		fputs("Malloc was unable to allocate more memory!", stderr);
@@ -39,8 +40,38 @@ inline CharString* _concat(CharString* lhs, CharString* rhs)
 	memcpy(ret->data + lhs_length, rhs->data, rhs_length);
 
 	return ret;
+}
 
+inline int _compare(CharString* lhs, CharString* rhs)
+{
+	if (lhs->length != rhs->length)
+	{
+		return 1;
+	}
 
+	for (size_t i = 0; i < lhs->length; ++i)
+	{
+		if (lhs->data[i] != rhs->data[i])
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+inline int _toInt(CharString* str)
+{
+	int ret = strtol(str->data, NULL, 10);
+
+	return ret;
+}
+
+inline int _toDouble(CharString* str)
+{
+	int ret = strtod(str->data, NULL);
+
+	return ret;
 }
 
 inline CharString* _getLine(FILE* fp, size_t size, int c) {
@@ -48,9 +79,9 @@ inline CharString* _getLine(FILE* fp, size_t size, int c) {
 	if (!c)
 		c = '\n';
 
+	//Assuming 32 bit system
 	if (size < 32)
 		size = 32;
-
 
 	CharString* ret = NULL;
 	ret = malloc(sizeof(CharString));
@@ -75,7 +106,7 @@ inline CharString* _getLine(FILE* fp, size_t size, int c) {
 		ret->data[len++] = ch;
 		if (len == size)
 		{
-			tmp = realloc(ret->data, sizeof(char) * (size += size));
+			tmp = realloc(ret->data, sizeof(char)*(size += size));
 			if (tmp)
 			{
 				ret->data = tmp;
@@ -88,13 +119,12 @@ inline CharString* _getLine(FILE* fp, size_t size, int c) {
 			}
 		}
 	}
-
+	free(tmp);
 	ret->data[len] = '\0';
 
-	memcpy(&ret->length, &len, sizeof(size_t));
-	ret->data = realloc(ret->data, sizeof(char) * len + 1);
-	free(tmp);
-
+	ret->length = len;
+	ret->data = realloc(ret->data, sizeof(char)*len+1);
+	
 	return ret;
 }
 
@@ -106,21 +136,28 @@ int main(void) {
 	puts("PLEASE ENTER THE FIRST NAME");
 	str = _getLine(stdin, NULL, NULL);
 	memcpy(&p->first, str, sizeof(CharString));
-	printf("%s\n", str->data);
+	printf("data:%s\nlength:%i\n\n", str->data, str->length);
 
 	puts("PLEASE ENTER THE LAST NAME");
 	str = _getLine(stdin, NULL, NULL);
 	memcpy(&p->last, str, sizeof(CharString));
+	printf("data:%s\nlength:%i\n\n", str->data, str->length);
 
-	p->id = 1;
+	puts("PLEASE ENTER THE PERSON ID");
+	str = _getLine(stdin, NULL, NULL);
+	p->id = _toInt(str);
+
 	printf("%i %s %s\n", p->id, p->first.data, p->last.data);
 
 	str = _concat(&p->first, &p->last);
 	printf("CONCAT FIRST AND LAST: %s\n", str->data);
 	free(str);
 
-	printf("%i %s %s\n", p->id, p->first.data, p->last.data);
-
+	if (_compare(&p->first, &p->last))
+	{ 
+		printf("COMPARE DETERMINED THE NAMES WERE DIFFERENT\n"); 
+	}
+	free(p);
 	system("pause");
 	return EXIT_SUCCESS;
 }
