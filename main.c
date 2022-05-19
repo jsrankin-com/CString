253fad39484216b8eb106f94cc6b441aa4215c09
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-05-19 00:10:58 -0400

    Update main.c
    Not fixed yet - error on clear

--- main.c
+++ main.c
@@ -1,163 +1,184 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct
 {
+	struct CharString* ptr;
 	size_t length;
+	//size_t size;
+
+	size_t(*ToSizeT)(void* ptr);
+	long double (*ToDouble)(void* ptr);
+	void (*PrintCharString)(void* ptr);
+	void (*ClearCharString)(void* ptr);
+	void (*DeleteCharString)(void* ptr);
+	void* (*GetLine)(FILE* fp, size_t size, int c);
+
 	char* data;
 } CharString;
 
+
+
+size_t _ToSizeT(const CharString* self)
+{
+	//convert to size_t
+	return strtoull(self->data, self->data[self->length], 10);
+}
+
+long double _ToDouble(const CharString* self)
+{
+	//convert to size_t
+	return strtold(self->data, self->data[self->length], 10);
+}
+
+void _PrintCharString(const CharString* self)
+{
+	printf("Length:%zu\nData:%s\nPointer:%p\n", self->length, self->data, self->ptr);
+}
+
+void _ClearCharString(CharString* self)
+{
+	if (!self || !(self->ptr = realloc(self->data, sizeof(char))))
+	{
+		fputs("Realloc was unable to reclaim memory!", stderr);
+		exit(EXIT_FAILURE);
+	}
+	self->data[0] = '\0';
+	self->length = 0;
+}
+
+void _DeleteCharString(CharString* self)
+{
+	self->ClearCharString(self);
+
+	free(self);
+}
+
+
+
 typedef struct
 {
-	unsigned id;
+	size_t id;
 	CharString first;
 	CharString last;
-	
+
 } Person;
+
 
 inline CharString* _concat(CharString* lhs, CharString* rhs)
 {
 	size_t lhs_length = lhs->length;
-	//Includes \0
-	size_t rhs_length = rhs->length + 1;
-	size_t size = lhs_length + rhs_length;
+	size_t rhs_length = rhs->length;
+	size_t length = lhs_length + rhs_length;
 	CharString* ret = NULL;
 	ret = malloc(sizeof(CharString));
 	if (!ret)
 	{
 		fputs("Malloc was unable to allocate more memory!", stderr);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
-	ret->data = malloc(sizeof(char)*size);
+	ret->data = malloc(sizeof(char) * (length + 1));
 	if (!ret->data)
 	{
 		fputs("Malloc was unable to allocate more memory!", stderr);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
+
+	//Prefer assignment
+	ret->data[length] = '\0';
+	ret->length = length;
 
 	memcpy(ret->data, lhs->data, lhs_length);
 	memcpy(ret->data + lhs_length, rhs->data, rhs_length);
-
-	return ret;
-}
-
-inline int _compare(CharString* lhs, CharString* rhs)
-{
-	if (lhs->length != rhs->length)
-	{
-		return 1;
-	}
-
-	for (size_t i = 0; i < lhs->length; ++i)
-	{
-		if (lhs->data[i] != rhs->data[i])
-		{
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-inline int _toInt(CharString* str)
-{
-	int ret = strtol(str->data, NULL, 10);
-
-	return ret;
-}
-
-inline int _toDouble(CharString* str)
-{
-	int ret = strtod(str->data, NULL);
-
+	ret->ptr = &ret;
 	return ret;
 }
 
 inline CharString* _getLine(FILE* fp, size_t size, int c) {
 
-	if (!c)
-		c = '\n';
-
-	//Assuming 32 bit system
-	if (size < 32)
-		size = 32;
+	//Default value if none supplied
+	c = (!c) ? '\n' : c;
+	size = (size < 32) ? 32 : size;
+	size_t buffer = size;
 
 	CharString* ret = NULL;
 	ret = malloc(sizeof(CharString));
 	if (!ret)
 	{
 		fputs("Malloc was unable to allocate more memory!", stderr);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
-	ret->data = malloc(sizeof(char) * size);
+	ret->data = malloc(sizeof(char) * buffer);
 	if (!ret->data)
 	{
 		fputs("Malloc was unable to allocate more memory!", stderr);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	int ch = 0;
 	size_t len = 0;
 	char* tmp = NULL;
-	while ((ch = fgetc(fp)) != EOF && ch != c)
+	while (EOF != (ch = fgetc(fp))
+		&& ch != c)
 	{
 		ret->data[len++] = ch;
-		if (len == size)
+		if (len == buffer)
 		{
-			tmp = realloc(ret->data, sizeof(char)*(size += size));
-			if (tmp)
-			{
-				ret->data = tmp;
-				tmp = NULL;
-			}
-			else
+			tmp = realloc(ret->data, sizeof(char) * (buffer += size));
+			if (!tmp)
 			{
 				fputs("Realloc was unable to allocate more memory!", stderr);
-				return EXIT_FAILURE;
+				exit(EXIT_FAILURE);
 			}
+			ret->data = tmp;
+			tmp = NULL;
 		}
 	}
-	free(tmp);
-	ret->data[len] = '\0';
 
+	//Prefer assignment
+	ret->data[len] = '\0';
 	ret->length = len;
-	ret->data = realloc(ret->data, sizeof(char)*len+1);
-	
+
+	//Attempt to reclaim memory
+	if (!realloc(ret->data, sizeof(char) * (len + 1)))
+	{
+		fputs("Realloc was unable to reclaim memory!", stderr);
+		exit(EXIT_FAILURE);
+	}
+	ret->ptr = ret;
+	free(tmp);
+
+
+	ret->PrintCharString = _PrintCharString;//function pointer
+	ret->ToSizeT = _ToSizeT;
+	ret->ToDouble = _ToDouble;
+	ret->ClearCharString = _ClearCharString;
+	ret->DeleteCharString = _DeleteCharString;
+	ret->GetLine = _getLine;
+
 	return ret;
 }
 
+
 int main(void) {
-	CharString* str = NULL;
-	Person* p = NULL;
-	p = malloc(sizeof(Person));
 
-	puts("PLEASE ENTER THE FIRST NAME");
-	str = _getLine(stdin, NULL, NULL);
-	memcpy(&p->first, str, sizeof(CharString));
-	printf("data:%s\nlength:%i\n\n", str->data, str->length);
+	Person pp;
 
-	puts("PLEASE ENTER THE LAST NAME");
-	str = _getLine(stdin, NULL, NULL);
-	memcpy(&p->last, str, sizeof(CharString));
-	printf("data:%s\nlength:%i\n\n", str->data, str->length);
 
-	puts("PLEASE ENTER THE PERSON ID");
-	str = _getLine(stdin, NULL, NULL);
-	p->id = _toInt(str);
 
-	printf("%i %s %s\n", p->id, p->first.data, p->last.data);
+	pp.first = *_getLine(stdin, NULL, NULL);
+	pp.last = *_getLine(stdin, NULL, NULL);
+	pp.id = _ToSizeT(_getLine(stdin, NULL, NULL));
 
-	str = _concat(&p->first, &p->last);
-	printf("CONCAT FIRST AND LAST: %s\n", str->data);
-	free(str);
+	printf("%p\n%p\n%p\n\n", pp.first.ptr, &pp.first, &pp);
 
-	if (_compare(&p->first, &p->last))
-	{ 
-		printf("COMPARE DETERMINED THE NAMES WERE DIFFERENT\n"); 
-	}
-	free(p);
+
+	pp.first.DeleteCharString(pp.first.ptr);
+
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
